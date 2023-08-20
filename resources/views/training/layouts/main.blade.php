87da2f0002dfe89c44b346a070330a3d7b2aa803
IvanGostev <ivangostev07@gmail.com>
2023-08-20 13:14:43 +0300

    UpdateStatusHometasks

--- resources/views/training/layouts/main.blade.php
+++ resources/views/training/layouts/main.blade.php
@@ -94,8 +94,6 @@
 <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('dist/js/demo.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
 <script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
